452742e117c3644a76775548f803c9ab6ee738ff
Clara690 <clara80416@gmail.com>
2026-07-31 20:21:18 +0200

    Added cleansing logic

--- scripts/silver/silver_load.sql
+++ scripts/silver/silver_load.sql
@@ -2,7 +2,7 @@ WITH depulicated_crm_cust_info AS (
     SELECT 
         *,
         ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS row_num
-    FROM `crm_cust_info`
+    FROM `dw_bronze.crm_cust_info`
 )
 SELECT cst_id,
 	   cst_key,
@@ -26,9 +26,66 @@ WHERE row_num = 1 AND cst_id != 0;
 SELECT
 	prd_id,
     prd_key,
-    SUBTRING(prd_key, 1, 5) AS cat_id,
+    REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract category key, the first 4 digit
+    SUBSTRING(prd_key, 7) AS prd_key_cleaned, -- Extract product key, starting from the 7th to the end 
     prd_nm,
-    prd_cost,
-    prd_line,
-    prd_start_dt,
-    prd_end_dt
+    IFNULL(prd_cost, 0) as prd_cost,
+    CASE UPPER(LTRIM(prd_line))
+		WHEN 'M' THEN 'Mountain'
+		WHEN 'R' THEN 'Road'
+		WHEN 'S' THEN 'Other Sales'
+		WHEN 'T' THEN 'Touring'
+		ELSE 'n/a'
+	END AS prd_line,
+    CAST(prd_start_dt AS DATE) AS prd_start_dt, 
+    CAST(
+    DATE_SUB(
+        LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt), 
+        INTERVAL 1 DAY
+    ) AS DATE
+) AS prd_end_dt_calculated -- Calculate end date as one day before the next start date
+FROM dw_bronze.crm_prd_info;
+
+SELECT
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    CASE WHEN sls_order_dt = 0
+        OR LENGTH(sls_order_dt) != 8 THEN
+        NULL
+    ELSE
+        CAST(CAST(sls_order_dt AS char) AS DATE)
+    END AS sls_order_dt,
+    CASE WHEN sls_ship_dt = 0
+        OR LENGTH(sls_ship_dt) != 8 THEN
+        NULL
+    ELSE
+        CAST(CAST(sls_ship_dt AS char) AS DATE)
+    END AS sls_ship_dt,
+    CASE WHEN sls_due_dt = 0
+        OR length(sls_due_dt) != 8 THEN
+        NULL
+    ELSE
+        CAST(CAST(sls_due_dt AS char) AS DATE)
+    END AS sls_due_dt,
+    CASE WHEN sls_sales IS NULL
+        OR sls_sales <= 0
+        OR sls_quantity * ABS(sls_price) THEN
+        sls_quantity * ABS(sls_price)
+    ELSE
+        sls_sales
+    END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
+    CASE WHEN sls_price IS NULL
+        OR sls_price <= 0 THEN
+        sls_sales / NULLIF (sls_quantity, 0)
+    ELSE
+        sls_price
+    END AS sls_price
+FROM
+    dw_bronze.crm_sales_details;
+
+
+
+
+
+
